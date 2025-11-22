3054ac0c5bb11ccdb897294a150e6a98bc5ac453
MuhammadRayhanTriPutra <muhammadrayhantri@student.esaunggul.ac.id>
2025-11-22 11:36:36 +0700

    18-09-2025

---
+++ pert1_Logic-Gate/assignment.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    x = 15;
+    cout << "Nilai dari variabel x adalah: " << x << endl;
+
+    int y = 20;
+    int z = x + y;
+
+    cout << "Nilai dari variabel y adalah: " << y << endl;
+    cout << "Hasil penjumlahan x + y = " << z << endl;
+
+    return 0;
+}
---
+++ pert1_Logic-Gate/output.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World" << endl;
+
+    int a = 5, b = 3;
+    cout << "Hasil dari" << a <<" + "<< b <<" = " << (a + b) << endl;
+
+    return 0;
+}
